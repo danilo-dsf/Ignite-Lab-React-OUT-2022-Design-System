5ca9c131553aeeae7b95fdc48a6c9680d21cb31f
Danilo Ferreira <danilo1705@gmail.com>
2022-10-15 17:12:18 -0300

    install tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import './styles/global.css';
+
 export function App() {
   return (
-    <h1>Hello world</h1>
+    <>
+      <h1 className='font-bold text-5xl text-violet-700'>Hello world</h1>
+
+      <button type='button' className='bg-violet-500 font-medium px-4 py-2 rounded text-white hover:bg-violet-600'>
+        Enviar
+      </button>
+    </>
   )
 }
