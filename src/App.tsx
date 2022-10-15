02b96005e0c4f7f51f64019f6d4582bedd0bd020
Danilo Ferreira <danilo1705@gmail.com>
2022-10-15 17:44:39 -0300

    create style tokens

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,9 @@ import './styles/global.css';
 export function App() {
   return (
     <>
-      <h1 className='font-bold text-5xl text-violet-700'>Hello world</h1>
+      <h1 className='font-bold text-2xl text-black'>Hello world</h1>
 
-      <button type='button' className='bg-violet-500 font-medium px-4 py-2 rounded text-white hover:bg-violet-600'>
+      <button type='button' className='bg-cyan-500 font-medium px-4 py-2 rounded text-white hover:bg-violet-600'>
         Enviar
       </button>
     </>
