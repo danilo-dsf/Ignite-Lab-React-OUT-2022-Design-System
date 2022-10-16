cff14b907ed4cc36b1fdb303537dac798029d4ca
Danilo Ferreira <danilo1705@gmail.com>
2022-10-16 17:36:25 -0300

    ci: add publish storybook workflow

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -14,5 +14,12 @@ module.exports = {
   },
   "features": {
     "storyStoreV7": true
+  },
+  viteFinal: (config, { configType }) => {
+    if (configType === 'PRODUCTION') {
+      config.base = '/Ignite-Lab-React-OUT-2022-Design-System/'
+    }
+
+    return config;
   }
 }
